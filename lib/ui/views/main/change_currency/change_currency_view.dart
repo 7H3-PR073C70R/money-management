2a336d34e580144c75c78b02d335539967fc9ed1
toxic-bishop <okanlawon.daniel@yahoo.com>
2021-10-29 06:59:37 +0100

    fix

--- lib/ui/views/main/change_currency/change_currency_view.dart
+++ lib/ui/views/main/change_currency/change_currency_view.dart
@@ -1,7 +1,6 @@
 import 'package:box_ui/box_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:money_management/constants/app_string.dart';
-import 'package:money_management/constants/enums.dart';
 import 'package:money_management/ui/shared/const_ui_helper.dart';
 import 'package:money_management/ui/shared/dumb_widgets/radio.dart';
 import 'package:stacked/stacked.dart';
